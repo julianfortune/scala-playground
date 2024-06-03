bb9f25fd1441c286fa456e35cc5a10777c0f998c
Julian Fortune <juliandfortune@gmail.com>
2024-06-02 18:47:17 -0700

    Adds basic tests

--- src/test/scala/cruise/cabin_price/BestGroupPricesTests.scala
+++ src/test/scala/cruise/cabin_price/BestGroupPricesTests.scala
@@ -4,10 +4,51 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class BestGroupPriceTests extends AnyFunSuite:
 
-  test("a test") {
-    assert(true)
+  test("produces the correct output for the example data") {
+    val inputRates = Seq(
+      Rate("M1", "Military"),
+      Rate("M2", "Military"),
+      Rate("S1", "Senior"),
+      Rate("S2", "Senior")
+    )
+
+    val inputCabins = Seq(
+      CabinPrice("CA", "M1", 200.00),
+      CabinPrice("CA", "M2", 250.00),
+      CabinPrice("CA", "S1", 225.00),
+      CabinPrice("CA", "S2", 260.00),
+      CabinPrice("CB", "M1", 230.00),
+      CabinPrice("CB", "M2", 260.00),
+      CabinPrice("CB", "S1", 245.00),
+      CabinPrice("CB", "S2", 270.00)
+    )
+
+    val expectedOutput = Seq(
+      BestGroupPrice("CA", "M1", 200.00, "Military"),
+      BestGroupPrice("CA", "S1", 225.00, "Senior"),
+      BestGroupPrice("CB", "M1", 230.00, "Military"),
+      BestGroupPrice("CB", "S1", 245.00, "Senior")
+    )
+
+    assert(getBestGroupPrices(inputRates, inputCabins) == expectedOutput)
   }
 
-  test("another test")(pending)
+  test("does not throw when there are unrecognized rate codes") {
+    val rates = Seq(
+      Rate("M1", "Military")
+    )
+
+    val cabins = Seq(
+      CabinPrice("CA", "M1", 200.00),
+      CabinPrice("CA", "M2", 250.00),
+      CabinPrice("CA", "S1", 225.00)
+    )
+
+    val expectedOutput = Seq(
+      BestGroupPrice("CA", "M1", 200.00, "Military")
+    )
+
+    assert(getBestGroupPrices(rates, cabins) == expectedOutput)
+  }
 
 end BestGroupPriceTests
